f59a805e5efa3e69797cc820bbd582a4a8acf7a3
jeffrr1 <64003277+jeffrr1@users.noreply.github.com>
2022-06-08 11:16:41 +0700

    1

---
+++ framework/src/components/ProductList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ProductList = () => {
+  return (
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        
+                    </th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+  )
+}
+
+export default ProductList
